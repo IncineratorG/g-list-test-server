ae88fd2b8eadb74fc99d4a1546cb0feee994c5e2
Alexander <alexander.v.dorohov@yandex.ru>
2020-04-16 17:50:14 +0300

    Для манипуляций списком покупок используется email пользователей.

--- functions/src/handlers/remove-shopping-list/removeShoppingList.js
+++ functions/src/handlers/remove-shopping-list/removeShoppingList.js
@@ -1,5 +1,6 @@
 const statusTypes = require('../../data/common/statusTypes');
 const firebasePaths = require('../../helpers/firebase-paths/firebasePaths');
+const idManager = require('../../helpers/id-manager/idManager');
 
 exports.removeShoppingListHandler = async ({req, res, admin}) => {
     const requestData = req.body;
@@ -12,52 +13,63 @@ exports.removeShoppingListHandler = async ({req, res, admin}) => {
         return;
     }
 
+    // Путь до создателя списка.
     const listSenderPath = firebasePaths.getPath({
         pathType: firebasePaths.paths.SHOPPING_LIST_SENDER,
         shoppingListId,
     });
+    // Путь до получателей списка.
     const listReceiversPath = firebasePaths.getPath({
         pathType: firebasePaths.paths.SHOPPING_LIST_RECEIVERS,
         shoppingListId,
     });
 
+    // Получаем данные создателя списка.
     const senderData = await admin
         .database()
         .ref(listSenderPath)
         .once('value');
+    // Получаем данные получателей списка.
     const receiversData = await admin
         .database()
         .ref(listReceiversPath)
         .once('value');
 
-    const senderPhone = senderData.val();
-    const receiversPhones = [];
+    // Получаем ID создателя и получателей списка.
+    const senderId = idManager.getId(senderData.val());
+    const receiversIds = [];
     receiversData.forEach(child => {
-        receiversPhones.push(child.val());
+        receiversIds.push(
+            idManager.getId(child.val())
+        );
     });
 
     let updates = {};
 
+    // Убираем список из пвпки создателя данного списка.
     const userSendPath = firebasePaths.getPath({
         pathType: firebasePaths.paths.USER_SEND_DELIM,
-        userId: senderPhone,
+        userId: senderId,
     });
     updates[userSendPath + shoppingListId] = null;
 
-    receiversPhones.forEach(receiverPhone => {
+    // Убираем список из папок получателей этого списка.
+    receiversIds.forEach(receiverId => {
         const userReceivedPath = firebasePaths.getPath({
             pathType: firebasePaths.paths.USER_RECEIVED_DELIM,
-            userId: receiverPhone,
+            userId: receiverId,
         });
         updates[userReceivedPath + shoppingListId] = null;
     });
 
+    // Удаляем сам список покупок.
     const shoppingListPath = firebasePaths.getPath({
         pathType: firebasePaths.paths.SHOPPING_LIST_DATA,
         shoppingListId,
     });
     updates[shoppingListPath] = null;
 
+    // Применяем обновление.
     await admin.database().ref().update(updates);
 
     res.json({
