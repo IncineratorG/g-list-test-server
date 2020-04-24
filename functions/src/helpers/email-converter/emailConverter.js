b9dab1ff8c2ae420a96aef7e0eebb4117555f092
Alexander <alexander.v.dorohov@yandex.ru>
2020-04-24 21:58:15 +0300

    Исправил способ кодирования электронной почты пользователя.

--- functions/src/helpers/email-converter/emailConverter.js
+++ functions/src/helpers/email-converter/emailConverter.js
@@ -1,14 +1,47 @@
 exports.encodeEmail = userEmail => {
-    return userEmail.replace(atSign, atReplacer).replace(dotSignGlobal, dotReplacer);
+    return userEmail
+        .replace(atSign, atReplacer)
+        .replace(dotSignGlobal, dotReplacer)
+        .replace(shiftSignGlobal, shiftReplacer)
+        .replace(dollarSignGlobal, dollarReplacer)
+        .replace(openBracketSignGlobal, openBracketReplacer)
+        .replace(closeBracketSignGlobal, closeBracketReplacer);
 };
 
 exports.decodeEmail = encodedEmail => {
-  return encodedEmail.replace(atReplacer, atSign).replace(dotReplacerGlobal, dotSign);
+  return encodedEmail
+      .replace(atReplacer, atSign)
+      .replace(dotReplacerGlobal, dotSign)
+      .replace(shiftReplacerGlobal, shiftSign)
+      .replace(dollarReplacerGlobal, dollarSign)
+      .replace(openBracketReplacerGlobal, openBracketSign)
+      .replace(closeBracketReplacerGlobal, closeBracketSign);
 };
 
 const atSign = '@';
+const atReplacer = '@';
+
 const dotSign = '.';
 const dotSignGlobal = /\./g;
-const atReplacer = '@';
 const dotReplacer = '%';
 const dotReplacerGlobal = /%/g;
+
+const shiftSign = '#';
+const shiftSignGlobal = /#/g;
+const shiftReplacer = '*';
+const shiftReplacerGlobal = /\*/g;
+
+const dollarSign = '$';
+const dollarSignGlobal = /\^/g;
+const dollarReplacer = '^';
+const dollarReplacerGlobal = /\^/g;
+
+const openBracketSign = '[';
+const openBracketSignGlobal = /\[/g;
+const openBracketReplacer = '+';
+const openBracketReplacerGlobal = /\+/g;
+
+const closeBracketSign = ']';
+const closeBracketSignGlobal = /]/g;
+const closeBracketReplacer = '-';
+const closeBracketReplacerGlobal = /-/g;
